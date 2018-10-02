b004f5bd1ce430abcd93acf2e76bc976efcbba6d
Isaac Dominguez <isaac.dominguez@privalia.com>
2018-10-02 22:55:38 +0200

    [PATCH] Modify Helm repo use chartmuseum inside k8s

--- vars/k8sUpgradeBeta.groovy
+++ vars/k8sUpgradeBeta.groovy
@@ -2,9 +2,15 @@ def call(project, domain, extraValues = "") {
     chartName = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}"
     tagBeta = "${currentBuild.displayName}-${env.BRANCH_NAME}"
     addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}.${domain}"
+
+    sh """helm repo add \
+        chartmuseum http://chartmuseum-chartmuseum:8080"""
+
+    sh """helm repo update"""
+
     sh """helm upgrade \
         ${chartName.toLowerCase()} \
-        helm/${project} -i \
+        chartmuseum/${project} -i \
         --namespace default \
         --set image.tag=${tagBeta} \
         --set ingress.host=${addr.toLowerCase()} \
