b00d79315384f040e9d11b9cb423069eb1d1e6a7
Isaac Dominguez <isaac.dominguez@privalia.com>
2018-10-07 12:46:45 +0200

    [PATCH] k8sUpgradeBeta add authentication museum

--- vars/k8sUpgradeBeta.groovy
+++ vars/k8sUpgradeBeta.groovy
@@ -1,18 +1,22 @@
 def call(project, domain, extraValues = "") {
-    chartName = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}"
-    tagBeta = "${currentBuild.displayName}-${env.BRANCH_NAME}"
-    addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}.${domain}"
+    withCredentials([usernamePassword(credentialsId: "chartmuseum", usernameVariable: "USER", passwordVariable: "PASS")]) {
+        chartName = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}"
+        tagBeta = "${currentBuild.displayName}-${env.BRANCH_NAME}"
+        addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}.${domain}"
 
-    sh """helm repo add \
+        sh """helm repo add \
+        --username $USER \
+        --password $PASS \
         chartmuseum http://chartmuseum-chartmuseum:8080"""
 
-    sh """helm repo update"""
+        sh """helm repo update"""
 
-    sh """helm upgrade \
+        sh """helm upgrade \
         ${chartName.toLowerCase()} \
         chartmuseum/${project} -i \
         --namespace ${project}-build \
         --set image.tag=${tagBeta} \
         --set ingress.host=${addr.toLowerCase()} \
         ${extraValues}"""
+    }
 }
